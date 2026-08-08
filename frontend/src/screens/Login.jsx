51a1662234a35d5970bc30a4ea0f292e6a817e2f
Madhav Dhyani <dhyani.pankaj811@gmail.com>
2026-08-08 16:24:36 +0530

    Add axios configuration and update Login component to use axios; remove sensitive .env file

--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from '../config/axios';
 
 const Login = () => {
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();

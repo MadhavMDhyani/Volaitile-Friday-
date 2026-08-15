f76f2feab84ad7a5a37620c015af0d5d2bbc7658
Madhav Dhyani <dhyani.pankaj811@gmail.com>
2026-08-15 23:24:03 +0530

    Enhance Project screen: update header styling and add button for improved UI interaction

--- frontend/src/screens/project.jsx
+++ frontend/src/screens/project.jsx
@@ -8,7 +8,22 @@ const Project = () => {
   return (
     <main className="h-screen w-screen flex">
       <section className="left h-full min-w-60 bg-red-300">
-        <header className="flex justify-end p-4 w-full" />
+        <header className="flex justify-end p-2 px-4 w-full bg-slate-200" >
+       <button
+       className='p-2'
+       >
+
+       
+
+       <i className="ri-group-line">
+       </i>
+
+       </button>
+
+
+
+     </header>
+
       </section>
     </main>
   );

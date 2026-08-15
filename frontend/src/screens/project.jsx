2fe12501694681f86fc4c1ad17ec36ad96bceefa
Madhav Dhyani <dhyani.pankaj811@gmail.com>
2026-08-15 23:29:05 +0530

    Enhance Project screen: add conversation area and message input for improved user interaction

--- frontend/src/screens/project.jsx
+++ frontend/src/screens/project.jsx
@@ -8,6 +8,10 @@ const Project = () => {
   return (
     <main className="h-screen w-screen flex">
       <section className="left h-full min-w-60 bg-red-300">
+
+
+
+
         <header className="flex justify-end p-2 px-4 w-full bg-slate-200" >
        <button
        className='p-2'
@@ -23,6 +27,18 @@ const Project = () => {
 
 
      </header>
+
+     <div className="conversation-area">
+
+        <div className="message-box">
+           <input type="text" placeholder='Enter message' />
+        <button ><i class="ri-send-ins-line"></i>
+        
+        </button> 
+
+        </div>
+
+     </div>
 
       </section>
     </main>

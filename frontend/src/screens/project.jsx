3fab1ce5cb86df5025891a08807b5a7cc0fb689f
Madhav Dhyani <dhyani.pankaj811@gmail.com>
2026-08-16 17:41:27 +0530

    Refactor Project screen: improve side panel functionality and enhance layout structure

--- frontend/src/screens/project.jsx
+++ frontend/src/screens/project.jsx
@@ -1,29 +1,25 @@
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import React, {useState} from 'react'
 
 const Project = () => {
-  const location = useLocation();
+const location = useLocation();
+const [isSidePanelOpen, setIsSidePanelOpen] = useState(false)
 
   console.log(location.state);
 
   return (
     <main className="h-screen w-screen flex">
-      <section className="left flex flex-col h-full min-w-72  bg-slate-300">
-
-
-
+      <section className="left relative flex flex-col h-full min-w-72  bg-slate-300">
 
         <header className="flex justify-end p-2 px-4 w-full bg-slate-100" >
-       <button
+       <button 
+       onClick={() => setIsSidePanelOpen(!isSidePanelOpen)}
        className='p-2 '
        >
        <i className="ri-group-line">
        </i>
 
        </button>
-
-
-
-  
       </header>
      <div className="conversation-area flex-grow flex flex-col">
       <div className="message-box p-1 flex-grow flex flex-col gap-1">         
@@ -54,6 +50,13 @@ const Project = () => {
         </div>
       </div>
     </div>
+
+     <div
+       className={`sidePanel w-36 h-60 bg-red-600 absolute left-[-100] ${
+         isSidePanelOpen ? '-translate-x-0' : ''
+       }`}
+     ></div>
+
 
   </section>
 </main>
